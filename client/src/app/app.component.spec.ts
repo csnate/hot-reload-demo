665605230a1fd8bf5fb3a17eb4ea27551a4c3a52
Nate Tovo <ntovo@conversantmedia.com>
2019-09-05 16:51:40 -0500

    minor changes

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -16,19 +16,4 @@ describe('AppComponent', () => {
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     });
-
-    it(`should have as title 'cia'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('cia');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('.content span').textContent).toContain(
-            'cia app is running!'
-        );
-    });
 });
